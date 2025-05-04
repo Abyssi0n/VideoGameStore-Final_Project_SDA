6be9028c9d169f03aeebb533d5fcee40757c6d25
Abyssion <pilovinas@seznam.cz>
2025-05-04 14:09:05 +0200

    Profile User Form v1

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,49 @@
+import profile
+
 from django.contrib.auth.forms import UserCreationForm
+from django.db.transaction import atomic
+from django.forms import CharField, Textarea, PasswordInput
 
 
 class SignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        fields = ['username', 'email',
+        fields = ['username', 'nickname', 'biography', 'email',
                   'password1', 'password2']
 
         labels = {
-            'username': 'Uživatelské jméno',
+            'biography': 'Bio',
             'email': 'E-mail',
+
         }
+
+    nickname = CharField(
+        label='Nickname',
+        required=False
+    )
+
+    biography = CharField(
+        widget=Textarea,
+        label='Bio',
+        required=False
+    )
+
+    password1 = CharField(
+        widget=PasswordInput(attrs={'placeholder': 'Password'}),
+        label='Password'
+    )
+
+    password2 = CharField(
+        widget=PasswordInput(attrs={'placeholder': 'Password again'}),
+        label='Password again'
+    )
+
+    @atomic
+    def save(self, commit=True):
+        self.instance.is_active = True
+        user = super().save(commit)
+
+
+
+        if commit:
+            profile.save()
+        return user
